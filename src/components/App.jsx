61432bd439c0d47c44d165166e13c95f4fe34af0
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-05-22 23:11:49 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,20 +10,20 @@ import { Route, Routes } from 'react-router-dom';
 //import Navigation from '../Navigation/Navigation';
 import HomePage from '../pages/homePage/HomePage';
 import NotFoundPage from '../pages/notFoundPage/NotFoundPage';
-//import MovieCast from '../components/movieCast/MovieCast';
-//import MovieReviews  from '../components/movieReviews/MovieReviews';
-//import css from './App.module.css';
+import MovieCast from '../components/movieCast/MovieCast';
+import MovieReviews  from '../components/movieReviews/MovieReviews';
+import css from './App.module.css';
 export default function App ()  {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={< MoviesPage/>} />
-        {/* <Route path="/movies/:movieId" element={<MovieDetailPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailPage />} />
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
-        </Route>  */}
-        <Route path="*" element={< NotFoundPage/>} />
+        </Route> 
+        <Route path="*" element={<NotFoundPage/>} />
       </Routes>
     </div>
   );
