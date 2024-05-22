2f6826b666a2d0c3deb07a672ff91033e3b6524f
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-05-22 23:39:44 +0300

    add

--- src/Api.js
+++ src/Api.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.unsplash.com/";
-export const getImages = async (topic, currentPage) => {
-  const response = await axios.get(
-    "/search/photos?client_id=E5ABb0U9uZIG-WiJXhb3ZD8kkLohYHcuGEKhpPQZUBg",
-    {
-      params: {
-        query: topic,
-        page: currentPage,
-        per_page: 12,
-      },
-    }
-  );
+// axios.defaults.baseURL = "https://65c23f3af7e6ea59682af8d1.mockapi.io";
 
-  return response.data;
+// export const getPayments = async () => {
+//   const response = await axios.get("/payments");
+//   return response.data;
+// };
+
+// export const getPaymentById = async (paymentId) => {
+//   const response = await axios.get(`/payments/${paymentId}`);
+//   return response.data;
+// };
+
+const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+
+const options = {
+  headers: {
+	// Замість api_read_access_token вставте свій токен
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjFmMTdlNjI0OGZhNjNkODEzN2MxZjIzYjAwZTQ1ZSIsInN1YiI6IjY2NGUxODA3NDMxNjhhODYyOTM0YTA4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8xl0O9U7-9Eursvybh2VWOuMQ_IJiplDyDebO8HstaE'
+    
+  }
 };
+
+axios.get(url, options)
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,20 @@
 //import { useEffect, useState } from "react";
 //import React from "react";
-
-
-
-//import MovieDetailPage from "../pages/movieDetailPage/MovieDetailPage";
+import { Route, Routes } from 'react-router-dom';
+import MovieDetailPage from "../pages/movieDetailPage/MovieDetailPage";
 import MoviesPage from "../pages/moviesPage/MoviesPage";
 //import { BiMoviePlay } from "react-icons/bi";
-import { Route, Routes } from 'react-router-dom';
 //import Navigation from '../Navigation/Navigation';
 import HomePage from '../pages/homePage/HomePage';
 import NotFoundPage from '../pages/notFoundPage/NotFoundPage';
 import MovieCast from '../components/movieCast/MovieCast';
 import MovieReviews  from '../components/movieReviews/MovieReviews';
-import css from './App.module.css';
+//import css from './App.module.css';
 export default function App ()  {
   return (
-    <div>
+    <div className={css.container}>
+      
+      <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={< MoviesPage/>} />
@@ -23,7 +22,7 @@ export default function App ()  {
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route> 
-        <Route path="*" element={<NotFoundPage/>} />
+        <Route path="*" element={< NotFoundPage/>} />
       </Routes>
     </div>
   );
