fe724b1d3598b74ee13707e60291a329c2a1e0db
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-05-25 22:59:29 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,22 @@
-//import { useEffect, useState } from "react";
-//import React from "react";
+import {lazy, Suspense} from 'react'
 import { Route, Routes } from 'react-router-dom';
-import MovieDetailPage from "../pages/movieDetailPage/MovieDetailPage";
-import MoviesPage from "../pages/moviesPage/MoviesPage";
-//import { BiMoviePlay } from "react-icons/bi";
-import Navigation from '../components/navigation/Navigation';
-import HomePage from '../pages/homePage/HomePage';
-import NotFoundPage from '../pages/notFoundPage/NotFoundPage';
-import MovieCast from '../components/movieCast/MovieCast';
-import MovieReviews  from '../components/movieReviews/MovieReviews';
+import Loader from './components/loader/Loader.jsx';
+const MovieDetailPage = lazy(() => import('../pages/movieDetailPage/MovieDetailPage.jsx'));
+const MoviesPage = lazy(() => import('../pages/moviesPage/MoviesPage.jsx'));
+import Navigation from '../components/navigation/Navigation.jsx'
+const HomePage = lazy(() => import('../pages/homePage/HomePage.jsx'));
+import NotFoundPage from '../pages/notFoundPage/NotFoundPage.jsx'
+const MovieCast = lazy(() => import('../components/movieCast/MovieCast.jsx'));
+const MovieReviews = lazy(() => import('../components/movieReviews/MovieReviews.jsx'));
+
 import css from './App.module.css';
 
 export default function App ()  {
   return (
     <div className={css.container}>
       
-      <Navigation />
+      <Navigation /> 
+      <Suspense fallback={<Loader/>}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={< MoviesPage/>} />
@@ -24,7 +25,17 @@ export default function App ()  {
           <Route path="reviews" element={<MovieReviews />} />
         </Route> 
         <Route path="*" element={< NotFoundPage/>} />
-      </Routes>
+      </Routes> 
+       </Suspense>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
